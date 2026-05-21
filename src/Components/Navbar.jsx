c7ee8e7566a29f03ed922bc130b9d8b854564ae7
shawon2911 <hosenuzzaman2018@gmail.com>
2026-05-21 13:59:39 +0600

    user profile added on navbar and show conditionally

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,99 +1,119 @@
 "use client";
 
-
 import { authClient } from "@/lib/auth-client";
-import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@heroui/react";
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Label,
+} from "@heroui/react";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-    const { 
-        data: session, 
-        
-    } = authClient.useSession() 
-    // console.log(session);
-    const user = session?.user;
-    // console.log(user);
+  const { data: session } = authClient.useSession();
+  // console.log(session);
+  const user = session?.user;
+  // console.log(user);
+
+  const handleSignOut = async() => {
+    await authClient.signOut();
+  }
   return (
     <div className=" py-5 shadow bg-gray-800  text-orange-400">
       <nav className="flex items-center justify-between max-w-7xl mx-auto ">
         <div>
-          <h2 className="text-3xl"><span>Sport</span><span className="text-white">Nest</span></h2>
+          <h2 className="text-3xl">
+            <span>Sport</span>
+            <span className="text-white">Nest</span>
+          </h2>
         </div>
-        
 
-
-        <ul className="flex items-center gap-3">
-            
-          <li>
+        <div className="flex items-center gap-5">
+          <ul className="flex items-center gap-5">
+          <li className="hover:text-orange-300">
             <Link href={"/"}>Home</Link>
           </li>
-          <li>
+          <li className="hover:text-orange-300">
             <Link href={"/all-facilities"}>All Facilities</Link>
           </li>
-          <li>
+        </ul> 
+        
+       <div className="">
+        {
+          user ? <ul className="flex items-center gap-5">
+            <li className="hover:text-orange-300">
             <Link href={"/my-bookings"}>My Bookings</Link>
           </li>
 
-          <li>
+          <li className="hover:text-orange-300">
             <Link href={"/add-facility"}>Add Facility</Link>
           </li>
-          <li>
+          <li className="hover:text-orange-300">
             <Link href={"/manage-my-facilities"}>Manage Facilities</Link>
           </li>
-        
-          {/* <li>
-            <Link href={"/profile"}>Profile</Link>
-          </li> */}
-
-
-            <Dropdown placement="bottom-end">
-  <DropdownTrigger>
-    <Avatar
-      src={user?.image}
-      name={user?.name}
-      className="cursor-pointer"
-    />
-  </DropdownTrigger>
-
-  <DropdownMenu aria-label="Profile Actions">
-
-    <DropdownItem key="profile">
-      <Link href="/profile">Profile</Link>
-    </DropdownItem>
-
-    <DropdownItem key="bookings">
-      <Link href="/my-bookings">My Bookings</Link>
-    </DropdownItem>
-
-    <DropdownItem key="add">
-      <Link href="/add-facility">Add Facility</Link>
-    </DropdownItem>
-
-    <DropdownItem key="manage">
-      <Link href="/manage-my-facilities">Manage Facilities</Link>
-    </DropdownItem>
-
-    <DropdownItem key="logout" className="text-danger" color="danger">
-      <button onClick={handleSignOut}>Log Out</button>
-    </DropdownItem>
-
-  </DropdownMenu>
-</Dropdown>
-
-
-
-
-
-
 
           <li>
+            <Dropdown>
+            
+
+              <Button  className={' bg-orange-400 py-6 rounded-md'}>
+                <div className="flex items-center gap-2">
+                <Avatar size="sm">
+                  <Avatar.Image
+                    alt={user?.name}
+                    src={user?.image}
+                  />
+                  <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+                </Avatar>
+                <h4>{user?.name}</h4>
+              </div>
+              </Button>
+
+              <Dropdown.Popover>
+                <Dropdown.Menu
+                  className="text-orange-600"
+                  onAction={(key) => console.log(`Selected: ${key}`)}
+                >
+                  <Dropdown.Item>
+                    <Link href={"/my-bookings"}>My Bookings</Link>
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <Link href={"/add-facility"}>Add Facility</Link>
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <Link href={"/manage-my-facilities"}>
+                      Manage Facilities
+                    </Link>
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <Button onClick={handleSignOut}
+                      className={"bg-red-800 text-white rounded-none w-full"}
+                    >
+                      Log Out
+                    </Button>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown.Popover>
+            </Dropdown>
+          </li>
+          </ul> : 
+          <ul className="flex items-center gap-5">
+          <li className="hover:text-orange-300">
             <Link href={"/login"}>Login</Link>
           </li>
-          <li>
+          <li className="hover:text-orange-300">
             <Link href={"/signup"}>Sign Up</Link>
           </li>
         </ul>
+        }
+       </div>
+        </div>
+
+        
       </nav>
     </div>
   );
